4187d57d0a82a167e42dfa0e25f17f699ed43835
qhddn2643 <qhddn2643@gmail.com>
2021-04-23 02:20:56 -0400

    OCaml programming project

--- interpreter_dynamic/interpreter_dynamic.ml
+++ interpreter_dynamic/interpreter_dynamic.ml
@@ -33,21 +33,21 @@ let rec multi_step (env : environment) (e : exp) : (environment * exp) = match e
                     (match n1 with
                     | (env, Num i) -> let n2 = multi_step env e2 in
                                       (match n2 with
-                                      |  env, Num j -> (env, Num(i+j))
+                                      | (env, Num j) -> (env, Num(i+j))
                                       | _ -> env, TypeError)
                     | _ -> env, TypeError)                  
   | Mult(e1, e2) -> let n1 = multi_step env e1 in
                     (match n1 with
                     | (env, Num i) -> let n2 = multi_step env e2 in
                                       (match n2 with
-                                      |  env, Num j -> (env, Num(i*j))
+                                      | (env, Num j) -> (env, Num(i*j))
                                       | _ -> env, TypeError)
                     | _ -> env, TypeError)
   | Var varname -> let (y, t) = List.find (fun (y, t) -> y = varname) env in (env, t)
-  | Lambda(var, body) -> env, Lambda(var, body) 
+  | Lambda(var, body) -> (env, Lambda(var, body))
   | Apply(e1, e2) -> let e1' = try multi_step env e1 with _ -> env, TypeError in
                      (match e1' with
-                     | env, Lambda(var, body) -> env, Let(var, e2, body)
+                     | env, Lambda(var, body) -> multi_step ((var, e2)::env) body
                      | _ -> env, TypeError)
   | Let(x, e1, e2) -> multi_step ((x, e1)::env) e2
 
